1e0b26e63134631ad7efd35c6b877670f596fedf
Boston Rohan <bostonrohan@gmail.com>
2021-08-24 23:06:36 -0700

    finished styles

--- src/layout/Calculator/Calculator.js
+++ src/layout/Calculator/Calculator.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Screen from "./Screen/Screen";
 import Keypad from "./Keypad/Keypad";
+import Toggle from "./Toggle/Toggle";
 const calculator = () => (
   <main className="calculator">
+    <Toggle />
     <Screen />
     <Keypad />
   </main>
---
+++ src/layout/Calculator/Toggle/Toggle.js
@@ -0,0 +1,10 @@
+import React from "react";
+const toggle = () => (
+  <div className="toggle">
+    <label className="switch">
+      <input type="checkbox"></input>
+      <span className="slider round"></span>
+    </label>
+  </div>
+);
+export default toggle;
