41d3e7463a218858bb4378d86f5cf26207c39e04
Boston Rohan <bostonrohan@gmail.com>
2021-10-15 18:26:12 -0400

    fixed display overflow, optimized dark-light toggle

--- src/components/Toggle/themes.js
+++ src/components/Toggle/themes.js
@@ -4,14 +4,12 @@ function setTheme(themeName) {
   document.documentElement.className = themeName;
 }
 function keepTheme() {
-  if (localStorage.getItem("theme")) {
-    if (localStorage.getItem("theme") === "theme-dark") {
-      setTheme("theme-dark");
-    } else if (localStorage.getItem("theme") === "theme-light") {
-      setTheme("theme-light");
-    } else {
-      setTheme("theme-dark");
-    }
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-dark");
+  } else if (localStorage.getItem("theme") === "theme-light") {
+    setTheme("theme-light");
+  } else {
+    setTheme("theme-dark");
   }
 }
 module.exports = {
